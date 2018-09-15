988e357982c2aba8a642d6ed30a6b60c676c050f
Jacob Portukalian <jacob@astraspace.com>
2018-09-15 14:36:33 -0700

    scripting setup of vim environment

--- vim/install-vim.sh
+++ vim/install-vim.sh
@@ -1,1 +1,9 @@
+rm -rf ~/.vim
+mkdir -p ~/.vim
+
 cp .vimrc ~/
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-sensible.git
+
